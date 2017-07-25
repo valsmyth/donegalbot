fe6c37052ce23a009bbc4aaf1902ee9d23876db7
Valerie Smyth <valsmyth@gmail.com>
2017-07-25 13:31:06 +0100

    removed credentials

--- app.js
+++ app.js
@@ -8,15 +8,11 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
 });
 
 // Create chat connector for communicating with the Bot Framework Service
-/*var connector = new builder.ChatConnector({
+var connector = new builder.ChatConnector({
     appId: process.env.MICROSOFT_APP_ID,
     appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
-*/
-var connector = new builder.ChatConnector({
-    appId: d4b9df70-270c-46cf-b81b-8c9333dd1019,
-    appPassword: 4WZZb69yqQtBqAFOWSah8H4
-});
+
 // Listen for messages from users 
 server.post('/api/messages', connector.listen());
 
